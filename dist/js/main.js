2ea2a8154d9c82cb258a9c5e19a2df20651d9e07
ALamir55 <phell966@gmail.com>
2020-02-13 23:32:02 +0200

    Lessons 19,20

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,8 +4,46 @@ $(function () {
   $('.toggle-sidebar').on('click', function () {
     $('.content-area, .sidebar').toggleClass('no-sidebar');
   });
+  // Toggle Submenu (open and close)
   $('.toggle-submenu').on('click', function () {
     $(this).children('.fa-angle-right').toggleClass('down');
     $(this).next('.child-links').slideToggle();
   });
+  // Fullscreen Toggle (open and close)
+  $('.toggle-fullscreen').on('click', function () {
+    $(this).toggleClass('fullscreen');
+    if ($(this).hasClass('fullscreen')) {
+      openFullscreen();
+    } else {
+      closeFullscreen();
+    }
+  });
 });
+
+
+// Fullscreen Functions
+let elem = document.documentElement;
+// Fullscreen Open Function
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullscreen) {
+    elem.mozRequestFullscreen();
+  } else if (elem.webkitRequestFullscreen) {
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) {
+    elem.msRequestFullscreen();
+  }
+}
+// Fullscreen Close Function
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullscreen) {
+    document.mozCancelFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
